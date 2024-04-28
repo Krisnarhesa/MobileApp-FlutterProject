a32b6fd0888cdcbebb4519924933141e54be14b4
Krisna Rhesa <pradnyana.2205551073@student.unud.ac.id>
2024-04-29 01:13:25 +0800

    modified routes & add Getstorage init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:progmob_app/forgotpasswordpage.dart';
 import 'package:progmob_app/welcomepage.dart';
+import 'package:progmob_app/registerpage.dart';
+import 'package:progmob_app/loginpage.dart';
+import 'package:progmob_app/homepage.dart';
 
-void main() {
-  runApp(const MainApp());
+
+Future<void> main() async {
+  await GetStorage.init();
+  runApp(MainApp());
 }
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -10,7 +17,14 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: WelcomePage(),
+      routes: {
+        '/': (context) => WelcomePage(),
+        '/register': (context) => RegisterPage(),
+        '/login': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/forgotpassword': (context) => ForgotPasswordPage(),
+      },
+      initialRoute: '/',
       debugShowCheckedModeBanner: false,
     );
   }
