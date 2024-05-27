8efd042edbc14c3e2fca8470be1b71bc28a3ccfe
Krisna Rhesa <pradnyana.2205551073@student.unud.ac.id>
2024-05-27 16:30:09 +0800

    add history transaksi

---
+++ lib/TabunganPage.dart
@@ -0,0 +1,156 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+
+class TabunganPage extends StatefulWidget {
+  @override
+  _TabunganPageState createState() => _TabunganPageState();
+}
+
+class _TabunganPageState extends State<TabunganPage> {
+  dynamic anggota;
+  List<Map<String, dynamic>> _historiTransaksi = [];
+
+  final _storage = GetStorage();
+  final _dio = Dio();
+  final _apiUrl = 'https://mobileapis.manpits.xyz/api';
+
+  final _nomerIndukController = TextEditingController();
+  final _namaController = TextEditingController();
+
+  final Map<int, String> transaksiTypes = {
+    1: 'Saldo Awal',
+    2: 'Simpanan',
+    3: 'Penarikan',
+    4: 'Bunga Simpanan',
+    5: 'Koreksi Penambahan',
+    6: 'Koreksi Pengurangan',
+  };
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final args = ModalRoute.of(context)?.settings.arguments;
+    if (args != null && args is Map<String, dynamic>) {
+      setState(() {
+        anggota = args;
+        _nomerIndukController.text = anggota['nomor_induk'].toString();
+        _namaController.text = anggota['nama'];
+      });
+      getTabunganAnggota();
+    } else {
+      print('Error: Anggota is null or arguments are not valid');
+    }
+  }
+
+  // Method untuk mendapatkan data id anggota dan histori transaksi
+  void getTabunganAnggota() async {
+    final token = _storage.read('token');
+    final idAnggota = anggota['id'];
+
+    try {
+      final response = await _dio.get(
+        '$_apiUrl/tabungan/$idAnggota',
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+
+      if (response.statusCode == 200) {
+        print('Response Data: ${response.data}'); // Print the response data
+        final data = response.data['data'];
+        final tabungan = data['tabungan']; // Access the 'tabungan' key
+        if (tabungan is List) {
+          setState(() {
+            _historiTransaksi = List<Map<String, dynamic>>.from(tabungan);
+          });
+        } else {
+          print('Error: Data format is not correct');
+        }
+      } else {
+        print('Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tabungan Anggota'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(height: 20),
+            TextField(
+              controller: _nomerIndukController,
+              decoration: InputDecoration(
+                labelText: 'Nomor Induk',
+                labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400),
+                prefixIcon:
+                    Icon(Icons.numbers, color: Colors.blue.shade400, size: 20),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade400),
+                ),
+              ),
+              enabled: false,
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: _namaController,
+              decoration: InputDecoration(
+                labelText: 'Nama',
+                labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400),
+                prefixIcon:
+                    Icon(Icons.person, color: Colors.blue.shade400, size: 20),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade400),
+                ),
+              ),
+              enabled: false,
+            ),
+            SizedBox(height: 20),
+            Text('Histori Transaksi',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
+            _historiTransaksi.isEmpty
+                ? Center(
+                    child:
+                        CircularProgressIndicator()) // Menampilkan loading indicator selama memuat data
+                : ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: _historiTransaksi.length,
+                    itemBuilder: (context, index) {
+                      final transaksi = _historiTransaksi[index];
+                      final trxType = transaksiTypes[transaksi['trx_id']] ??
+                          'Tipe Transaksi Tidak Tersedia';
+                      return ListTile(
+                        title: Text(trxType),
+                        subtitle: Text(
+                            'Nominal: Rp.${transaksi['trx_nominal'] ?? 'Nominal Tidak Tersedia'}'),
+                        trailing: Text(transaksi['trx_tanggal'] ??
+                            'Tanggal Transaksi Tidak Tersedia'),
+                      );
+                    },
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+}
