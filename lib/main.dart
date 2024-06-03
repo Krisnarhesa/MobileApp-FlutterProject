04f487eb7cbab162572c55c57f263341897f3534
Krisna Rhesa <pradnyana.2205551073@student.unud.ac.id>
2024-06-03 17:24:08 +0800

    add route bunga

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:progmob_app/DetailAnggotaPage.dart';
+import 'package:progmob_app/EditAnggotaPage.dart';
+import 'package:progmob_app/TabunganPage.dart';
+import 'package:progmob_app/TransaksiPage.dart';
+import 'package:progmob_app/addanggotapage.dart';
+import 'package:progmob_app/bungapage.dart';
 import 'package:progmob_app/forgotpasswordpage.dart';
 import 'package:progmob_app/homepage.dart';
 import 'package:progmob_app/loginpage.dart';
 import 'package:progmob_app/profilepage.dart';
 import 'package:progmob_app/registerpage.dart';
 import 'package:progmob_app/welcomepage.dart';
-import 'package:progmob_app/addanggotapage.dart';
-import 'package:progmob_app/EditAnggotaPage.dart';
-import 'package:progmob_app/DetailAnggotaPage.dart';
-import 'package:progmob_app/TransaksiPage.dart';
-import 'package:progmob_app/TabunganPage.dart';
 
 Future<void> main() async {
   await GetStorage.init();
   runApp(MainApp());
 }
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
@@ -32,8 +34,9 @@ class MainApp extends StatelessWidget {
         '/addanggota': (context) => AddAnggotaPage(),
         '/editanggota': (context) => EditAnggotaPage(),
         '/detailanggota': (context) => DetailAnggotaPage(),
-        '/transaksi' : (context) => TransaksiPage(),
-        '/tabungan' : (context) => TabunganPage(),
+        '/transaksi': (context) => TransaksiPage(),
+        '/tabungan': (context) => TabunganPage(),
+        '/bunga': (context) => BungaPage(),
       },
       initialRoute: '/',
       debugShowCheckedModeBanner: false,
